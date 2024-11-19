09e7ee9371ba86b0f380f468d31c517265ce16c2
JASMIN <JASMIN@DESKTOP-Q54L9MU>
2024-11-19 11:42:04 +0530

    fifth push

---
+++ Jenkins_pratice/src/test/java/cars/LambarghiniTest.java
@@ -0,0 +1,21 @@
+package cars;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LambarghiniTest {
+	@Test
+	public void launch() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://www.lamborghini.com/en-en");
+		Reporter.log("Lamborghini launched successfully",true);
+	}
+
+}
